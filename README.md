38d572cc20dbe4364c38dcbeb2ffe1967dc2b35d
Mark Conroy <info@adesignforlife.net>
2016-12-23 13:03:42 +0000

    Begin afresh, afresh, afresh

---
+++ README.md
@@ -0,0 +1,17 @@
+# The Trees
+## By Philip Larkin
+
+The trees are coming into leaf
+Like something almost being said;
+The recent buds relax and spread,
+Their greenness is a kind of grief.
+
+Is it that they are born again
+And we grow old? No, they die too.
+Their yearly trick of looking new
+Is written down in rings of grain.
+
+Yet still the unresting castles thresh
+In fullgrown thickness every May.
+Last year is dead, they seem to say,
+Begin afresh, afresh, afresh.
---
+++ index.php
@@ -0,0 +1,17 @@
+<h1>The Trees</h1>
+<h2>By Philip Larkin</h2>
+
+<p>The trees are coming into leaf<br>
+Like something almost being said;<br>
+The recent buds relax and spread,<br>
+Their greenness is a kind of grief.</p>
+
+<p>Is it that they are born again<br>
+And we grow old? No, they die too.<br>
+Their yearly trick of looking new<br>
+Is written down in rings of grain.</p>
+
+<p>Yet still the unresting castles thresh<br>
+In fullgrown thickness every May.<br>
+Last year is dead, they seem to say,<br>
+Begin afresh, afresh, afresh.</p>
