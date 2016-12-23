63de97f019b553ccb1a948fa6f2b92d8c2162951
Mark Conroy <info@adesignforlife.net>
2016-12-23 15:35:08 +0000

    proof of concept - it works, yee haw

--- index.php
+++ index.php
@@ -1,17 +1,28 @@
-<h1>The Trees</h1>
-<h2>By Philip Larkin</h2>
+<?php
+  $emails = array('mark@contact.ie, info@contact.ie, help@contact.ie, list@contact.ie, new@contact.ie');
+  $subject = 'A subject line';
+  $message = 'This is my message to you, Rudy';
+?>
 
-<p>The trees are coming into leaf<br>
-Like something almost being said;<br>
-The recent buds relax and spread,<br>
-Their greenness is a kind of grief.</p>
-
-<p>Is it that they are born again<br>
-And we grow old? No, they die too.<br>
-Their yearly trick of looking new<br>
-Is written down in rings of grain.</p>
-
-<p>Yet still the unresting castles thresh<br>
-In fullgrown thickness every May.<br>
-Last year is dead, they seem to say,<br>
-Begin afresh, afresh, afresh.</p>
+<form action="mailto:
+  <?php foreach ($emails as $email): ?>
+    <?php print $email . ','; ?>
+  <?php endforeach; ?>" method="GET">
+  <div>
+    <p>Who do you want to email?</p>
+    <select>
+      <option>All TDs</option>
+      <option>All Senators</option>
+    </select>
+  <div>
+    <p>What is the Subject of the Email?</p>
+    <input name="subject" type="text" />
+  </div>
+  <p>Type your message here:</p>
+  <div>
+    <textarea name="body"></textarea>
+  </div>
+  <div>
+    <input type="submit" value="Send Email" />
+  </div>
+</form>
