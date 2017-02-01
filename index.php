2bccd9ddec4457a96b10b3ce030c213c6fccb755
Mark Conroy <info@adesignforlife.net>
2017-02-01 18:16:12 +0000

    begin frontpage architecture

--- index.php
+++ index.php
@@ -8,12 +8,28 @@
   <body>
 
     <h1>Welcome to Contact.ie</h1>
+    <h2>I want to contact all TDs from:</h2>
+    <div class="option-wrapper">
+      <button>A political party</button>
+      <div class="political-parties">
+        <button class="fianna-fail">Fianna Fail</button><br>
+        <button class="fine-gael">Fine Gael</button><br>
+        <button class="labour">Labour</button>
+      </div>
+    </div>
+    <div class="option-wrapper">
+      <button>A constituency</button>
+      <div class="political-parties">
+        <button class="fianna-fail">Laois/Offaly</button><br>
+        <button class="fine-gael">Fine Gael</button><br>
+        <button class="labour">Labour</button>
+      </div>
+    </div>
 
-    <button class="fianna-fail">Fianna Fail</button><br>
-    <button class="fine-gael">Fine Gael</button><br>
-    <button class="labour">Labour</button>
+
+
 
     <div id="form"><div>
-      
+
   </body>
 </html>
