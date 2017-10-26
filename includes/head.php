e0751984cfbbbd657391517150c49ac844452e60
Mark Conroy <info@adesignforlife.net>
2017-10-26 21:20:47 +0100

    correct path to service worker

--- includes/head.php
+++ includes/head.php
@@ -56,7 +56,7 @@
     <link rel="stylesheet" href="/css/style.css">
     <script>
       if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('service-worker.js')
+        navigator.serviceWorker.register('/service-worker.js')
           .then(() => console.log('service worker installed'))
           .catch(err => console.log('Error', err));
       }
