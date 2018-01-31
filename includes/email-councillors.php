a7bf226bce7aa1f27266747c2546eca37d12eab2
Peter Heylin <peter@peterheylin.com>
2018-01-31 17:10:05 +0000

    20180131 adding municipal districts feature

--- includes/email-councillors.php
+++ includes/email-councillors.php
@@ -45,18 +45,18 @@
     }
     print '?bcc=sent@contact.ie">Email All ' . $item['council_name'] . ' Councillors</a></li>';
 	
+	/*
 	$mundistricts = $item['district'];
 	print '<li>' . $item['council_name'] . '</li>';
 
-	if(!empty($mundistricts)){
-		for ($i=0; $i<count($mundistricts); $i++) {
-			print '<ul><li><a href="mailto:';
-			foreach ($mundistricts[$i] as $district) {
-				print $district . ',';
-			}
-			print '?bcc=sent@contact.ie">Email All ' . $mundistricts[$i] . ' Councillors</a></li>';
+	for ($i=0; $i<count($mundistricts); $i++) {
+		print '<ul><li><a href="mailto:';
+		foreach ($mundistricts[$i] as $district) {
+			print $district . ',';
 		}
-	  }
+		print '?bcc=sent@contact.ie">Email All ' . $mundistricts[$i] . ' Councillors</a></li>';
+	}
+   */
 
   }
  
