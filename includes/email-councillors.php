c86565575b638e70bf34409c5219593e0ce335de
Peter Heylin <peter@peterheylin.com>
2018-02-02 14:52:16 +0000

    20180202 adding municipal districts to counsillors page

--- includes/email-councillors.php
+++ includes/email-councillors.php
@@ -42,13 +42,16 @@
 	$mundistricts = $item['district'];
 	print '<li>' . $item['council_name'] . '</li>';
     print '<ul><li><a href="mailto:';
-	foreach ($mundistricts as $district => $email){
-		print $email . ',';
+	foreach ($mundistricts as $district => $emailAddr){
+		for ($i=0;$i<count($emailAddr);$i++){
+			print $emailAddr[$i] . ',';
+		}
+		print '?bcc=sent@contact.ie">Email All ' . $district . ' Councillors</a></li></ul>';
 	}
     //foreach ($email_addresses as $email_address) {
     //  print $email_address . ',';
     //}
-    print '?bcc=sent@contact.ie">Email All ' . $district . ' Councillors</a></li></ul>';
+    //print '?bcc=sent@contact.ie">Email All ' . $district . ' Councillors</a></li></ul>';
 	
 		
 	/*
